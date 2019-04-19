f0e5e84212fd2790368cd5fa3c1ae1a1f195c0b6
Dennis <dennsva@gmail.com>
2019-04-19 18:20:15 +0300

    osa1 tehtävä1

---
+++ osa1/01-kurssitiedot/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1}
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.part1} exercises={props.exercises1}/>
+            <Part part={props.part2} exercises={props.exercises2}/>
+            <Part part={props.part3} exercises={props.exercises3}/>
+        </div>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>yhteensä {props.total} tehtävää</p>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
